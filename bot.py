e6b46fd32d39ec02f5d6ee4632b7d49e96024f69
OMINDA DILSHAN RESPACT <85678266+omindadelshan@users.noreply.github.com>
2021-09-02 11:23:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ app = Client(
 
 @app.on_message(filters.private & filters.command(['start']))
 async def start(client, message):
-	await message.reply_text(text =f"👋Hello **{message.from_user.first_name }** \n\n __I am simple Google Translater Bot.. \n 👨‍💻I can translate any language to you selected language__",reply_to_message_id = message.message_id ,parse_mode="markdown", reply_markup=InlineKeyboardMarkup([ [                    InlineKeyboardButton("🇱🇰 Support Group 🇱🇰 " ,url="https://t.me/sdbotworld") ],               [InlineKeyboardButton("👨‍💻Developer👨‍💻", url="https://t.me/omindas") ]   ]  ) )
+	await message.reply_text(text =f"👋Hello **{message.from_user.first_name }** \n\n __I am simple Google Translater Bot.. \n 👨‍💻I can translate any language to you selected language__",reply_to_message_id = message.message_id ,parse_mode="markdown", reply_markup=InlineKeyboardMarkup([ [                    InlineKeyboardButton("🇱🇰 Support Group 🇱🇰 " ,url="https://t.me/tgbotschatzone") ],               [InlineKeyboardButton("Channal 📣", url="https://t.me/szbots") ]   ]  ) )
                   
 
 
